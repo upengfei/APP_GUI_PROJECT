ae64e3fe1e5d0e6fa8eb1b59d09d3d922aae0688
nick-yu <yu.pf@163.com>
2016-04-27 12:51:53 +0800

    api-test

---
+++ Gui_test.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+import PySelenium
+import unittest
+import HTMLTestRunner
+import time
+import HttpFunc
+import ReadFile
+import MysqlDB
+
+class QydTest(unittest.TestCase):
+
+    def setUp(self):
+        HttpFunc.HttpFunc().get_root_path()
+        self.ps = PySelenium.PySelenium()
+        self.rf = ReadFile.ReadFile()
+        self.md = MysqlDB.MysqlDB()
+
+    def tearDown(self):
+        self.ps.quit()
+
+    def test_open_qyd(self):
+
+        self.ps.open_url(self.rf.get_option_value("http","host"))
+        if self.ps.is_display(u'link_text=>用户登录'):
+            print u"正常打开轻易贷首页，开始进行登录"
+            self.ps.click_text(u'用户登录')
+        else:
+            print u'首页打开失败'
+
+
+
+if __name__=='__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(QydTest('test_open_qyd'))
+    now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+    HttpFunc.HttpFunc.create_report("test_gui")
+
+    filename = HttpFunc.HttpFunc.get_report("test_gui")
+    fp = file(filename, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u'测试报告', description=u'测试报告详情： ')
+    runner.run(suite)
