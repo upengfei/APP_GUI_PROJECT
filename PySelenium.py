469354b1a176e11541465d4e7732d4fcd65b1a52
nick-yu <yu.pf@163.com>
2016-04-27 12:51:33 +0800

    api-test

--- PySelenium.py
+++ PySelenium.py
@@ -11,7 +11,8 @@ import os,sys,time
 
 class PySelenium(object):
 
-    def __init(self, browser="ff"):
+    def __init__(self, browser="ff"):
+
         try:
             if browser == "firefox" or browser == "ff":
                 self.driver = webdriver.Firefox()
@@ -45,13 +46,13 @@ class PySelenium(object):
     def get_element(self, data_type):
         '''
 
-        :param data_type: data_type类型如下：css:#id,xpath://,id:kw,name:name,class:class
+        :param data_type: data_type类型如下：css=>#id,xpath=>//,id=>kw,name=>name,class=>class
         :return:
         '''
-        if ':' not in data_type:
+        if '=>' not in data_type:
             raise NameError(u"data_type格式错误，请输入正确的格式，如：css:#id")
-        type = data_type.split(':')[0]
-        value = data_type.split(':')[1]
+        type = data_type.split('=>')[0]
+        value = data_type.split('=>')[1]
 
         if type == 'id':
             element = self.driver.find_element_by_id(value)
@@ -71,10 +72,10 @@ class PySelenium(object):
 
     def element_wait(self, data_type, sec=3):
 
-        if ':' not in data_type:
-            raise NameError(u"data_type格式错误，请输入正确的格式，如：css:#id")
-        type = data_type.split(':')[0]
-        value = data_type.split(':')[1]
+        if '=>' not in data_type:
+            raise NameError(u"data_type格式错误，请输入正确的格式，如：css=>#id")
+        type = data_type.split('=>')[0]
+        value = data_type.split('=>')[1]
 
         if type == 'id':
             WebDriverWait(self.driver, sec, 1).until(ec.presence_of_element_located((By.ID, value)))
@@ -92,16 +93,34 @@ class PySelenium(object):
             raise NameError(u"找不到type类型，请输入正确的类型")
 
     def set_window_size(self, width, height, windowhandle='current'):
+        '''
+
+        :param width:
+        :param height:
+        :param windowhandle:
+        :return:
+        '''
 
         self.driver.set_window_size(width, height, windowHandle=windowhandle)
 
     def input_type(self, data_type, text):
+        '''
+        适用于填充框的输入
+        :param data_type:
+        :param text:
+        :return:
+        '''
         self.element_wait(data_type)
         el = self.get_element(data_type)
         el.clear()
         el.send_keys(text)
 
     def click(self, data_type):
+        '''
+        适用于点击控件
+        :param data_type:
+        :return:
+        '''
         self.element_wait(data_type)
         el = self.get_element(data_type)
         el.click()
@@ -157,7 +176,7 @@ class PySelenium(object):
         '''
         self.driver.find_element_by_link_text(text).click()
 
-    def quite(self):
+    def quit(self):
         '''
         关闭所有的窗口,退出driver
         :return:
@@ -235,3 +254,54 @@ class PySelenium(object):
         '''
 
         self.driver.get_screenshot_as_file(file_path)
+
+    def alert_accept(self):
+        '''
+        确认点击警告对话框
+        :return:
+        '''
+        self.driver.switch_to.alert.accept()
+
+    def alert_dismiss(self):
+        '''
+        关闭警告对话框
+        :return:
+        '''
+        self.driver.switch_to.alert.dismiss()
+
+    def switch_to_frame(self, css):
+        '''
+        根据css属性切换到对应frame框架
+        :param css:
+        :return:
+        '''
+        el = self.driver.find_element_by_css_selector(css)
+        self.driver.switch_to.frame(el)
+
+    def forward(self):
+        '''
+        控制当前窗口前进或者后退
+        :return:
+        '''
+        self.driver.forward()
+
+    def get_window_handle(self):
+        '''
+        获取当前窗口句柄
+        :return:
+        '''
+        return self.driver.current_window_handle
+
+    def switch_to_new_window(self, window_old_handle):
+        '''
+        适用于点击某个控件后，跳转到新的窗口,配合get_window_handle使用
+        :param window_old_handle:
+        :return:
+        '''
+
+        all_handles = self.driver.window_handles
+
+        for handle in all_handles:
+            if handle != window_old_handle:
+                self.driver._switch_to.window(handle)
+
