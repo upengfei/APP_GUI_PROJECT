96daef2a6af2e03976bb96a49e4bcffe73d0041b
nick-yu <yu.pf@163.com>
2016-05-24 14:20:24 +0800

    __init__ changed

--- func/Base64.py
+++ func/Base64.py
@@ -1,12 +1,13 @@
 import base64
 import configparser
-
+from func import otherFunc
 
 class BaseChange:
 
-    def __init__(self, filename):
+    def __init__(self):
+        file_path = otherFunc.Func.get_root_path()+r'/config/config.ini'
         cp = configparser.ConfigParser()
-        cp.read(filename)
+        cp.read(file_path)
         self.username = cp.get(section='user', option='username')
         self.passwd = cp.get(section='user', option='passwd')
 
